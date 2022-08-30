13956b2d0eadfc4f213ffd614ab6c29ee8038b90
dargen3 <dargen3@centrum.cz>
2022-08-30 14:08:49 +0200

    flask2

--- start.sh
+++ start.sh
@@ -1,3 +1,15 @@
+#  Source: ubuntu-jammy-x86_64 
+#  Flavor: standard.large
+#  Networks: 147-251-115-pers-proj-net
+
+# sudo apt update
+# sudo apt install git
+# sudo bash start.sh
+
+
+# https://techtutorguro.com/how-to-install-flask-on-ubuntu-22-04-with-apache/
+
+
 sudo apt update -y && apt upgrade -y
 sudo apt-get install python3-pip python3-venv
 sudo apt install apache2
@@ -8,9 +20,17 @@ cd /opt/flask/
 python3 -m venv venv
 source venv/bin/activate
 pip install flask
-cp start_scripts/app.py /opt/flask/
-cp start_scripts/flaskapp.wsgi /opt/flask/
-cp start_scripts/flask.conf /etc/apache2/sites-available/flask.conf
+cp /home/ubuntu/AlphaCharges/start_scripts/app.py .
+cp /home/ubuntu/AlphaCharges/start_scripts/flaskapp.wsgi .
+sudo rm -f /etc/apache2/sites-available/*
+cp /home/ubuntu/AlphaCharges/start_scripts/flask.conf /etc/apache2/sites-available/
+sudo chown -R www-data:www-data /opt
+sudo chmod o+rx flaskapp.wsgi 
+sudo chmod o+rx app.py 
+
+
 
 sudo a2ensite flask.conf
 sudo systemctl restart apache2
+
+# cat /var/log/apache2/error.log
