b2ff05eaaecfc233b42150b715a0ae4ceeb525b2
dargen3 <dargen3@centrum.cz>
2022-10-11 09:33:32 +0000

    updated start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#  Source: ubuntu-jammy-x86_64 
+#  Source: ubuntu-jammy-x86_64
 #  Flavor: standard.large
 #  Networks: 147-251-115-pers-proj-net
 
@@ -9,7 +9,7 @@
 #sudo apt update
 #sudo apt install git
 #sudo bash start.sh
-
+# git clone https://github.com/dargen3/AlphaCharges in /opt folder
 
 
 
@@ -18,22 +18,18 @@ sudo apt-get install python3-pip python3-venv
 sudo apt install apache2
 sudo apt-get install libapache2-mod-wsgi-py3
 
-mkdir -p /opt/flask
-cd /opt/flask/
 python3 -m venv venv
 source venv/bin/activate
-pip install flask
-cp /home/ubuntu/AlphaCharges/start_scripts/app/routes.py .
-cp /home/ubuntu/AlphaCharges/start_scripts/app/alpha_charges.wsgi .
+pip install flask requests scipy rdkit numba numpy pdb2pqr
 sudo rm -f /etc/apache2/sites-available/*
 cp /home/ubuntu/AlphaCharges/start_scripts/alpha_charges.conf /etc/apache2/sites-available/
 sudo chown -R www-data:www-data /opt
-sudo chmod o+rx flaskapp.wsgi 
-sudo chmod o+rx app.py 
+sudo chmod o+rx app/AlphaCharges.wsgi
+sudo chmod o+rx app/routes.py
 
 
 
-sudo a2ensite flask.conf
+sudo a2ensite AlphaCharges.conf
 sudo systemctl restart apache2
 
 # cat /var/log/apache2/error.log0
