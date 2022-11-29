0e212db05c732dcd8d22f9641dc645225b3fa7fd
dargen3 <dargen3@centrum.cz>
2022-11-29 10:43:25 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,28 +1,16 @@
-#  Source: ubuntu-jammy-x86_64
-#  Flavor: standard.large
-#  Networks: 147-251-115-pers-proj-net
-
-# https://techtutorguro.com/how-to-install-flask-on-ubuntu-22-04-with-apache/
-
-
-
-#sudo apt update
-#sudo apt install git
+# sudo apt update -y && sudo apt upgrade -y
+# sudo apt install git
 # sudo git clone https://github.com/dargen3/AlphaCharges in /opt folder
 
 
 
-sudo apt update -y && sudo apt upgrade -y
-sudo apt-get install python3-pip python3-venv
-sudo apt install apache2
-sudo apt-get install libapache2-mod-wsgi-py3
-sudo apt-get install openbabel
+
+sudo apt-get install python3-pip python3-venv apache2 libapache2-mod-wsgi-py3 openbabel
 
 sudo python3 -m venv venv
 source venv/bin/activate
 sudo chown -R ubuntu:ubuntu /opt
-pip install flask requests scipy numba numpy pdb2pqr sklearn
-pip install rdkit==2022.03.5
+pip install flask requests scipy numba numpy pdb2pqr scikit-learn rdkit==2022.03.5
 sudo rm -f /etc/apache2/sites-available/*
 sudo cp AlphaCharges.conf /etc/apache2/sites-available/
 sudo chown -R www-data:www-data /opt
@@ -30,11 +18,10 @@ sudo chmod o+rx app/AlphaCharges.wsgi
 sudo chmod o+rx app/routes.py
 
 
-
 sudo a2ensite AlphaCharges.conf
 sudo systemctl restart apache2
 
 # change routes for pdb2pqr30 and openbabel in routes.py
 
-# cat /var/log/apache2/error.log0
+# cat /var/log/apache2/error.log
 
