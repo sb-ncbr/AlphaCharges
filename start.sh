9fe4b82477bcda1cb4c9830e05913ae74d725ad0
dargen3 <dargen3@centrum.cz>
2023-01-19 12:59:58 +0100

    start_script

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@ sudo apt update -y && sudo apt upgrade -y
 sudo apt install git
 cd /opt
 sudo git clone https://github.com/dargen3/AlphaCharges
+git checkout stable
 sudo apt-get install python3-pip python3-venv apache2 libapache2-mod-wsgi-py3 gemmi
 sudo python3 -m venv venv
 source venv/bin/activate
@@ -12,10 +13,7 @@ sudo cp AlphaCharges/AlphaCharges.conf /etc/apache2/sites-available/
 sudo chown -R www-data:www-data /opt
 sudo chmod o+rx AlphaCharges/app/AlphaCharges.wsgi
 sudo chmod o+rx AlphaCharges/app/routes.py
-
-
 sudo a2ensite AlphaCharges.conf
-
 sudo a2enmod ssl
 sudo a2enmod brotli
 sudo a2enmod http2
