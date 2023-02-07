389196046260c01d2137b58f89f70ed5565e2d20
Dominik Tichy <tichydominik451@gmail.com>
2023-02-07 18:08:49 +0000

    feat: added molstar viewer

--- app/static/main.js
+++ app/static/main.js
@@ -1,97 +1,129 @@
-'use strict';
+"use strict";
 
+let charge;
+let molstar;
 
-const spinner = '<span class="spinner-border spinner-border-sm" role="status" ' +
-    'aria-hidden="true" style="animation-duration: 1.5s"></span>';
-
-function update_litemol_colors(min_color, max_color) {
-    LiteMolChargesViewerEventQueue.send("lm-set-default-color-scheme", {
-        minVal: min_color,
-        maxVal: max_color,
-        fallbackColor: {r: 0, g: 255, b: 0},
-        minColor: {r: 255, g: 0, b: 0},
-        maxColor: {r: 0, g: 0, b: 255},
-        middleColor: {r: 255, g: 255, b: 255}
-    });
+function init_results(structure_url, id) {
+    (async () => {
+        molstar = await MolstarPartialCharges.create("root");
+        await load(structure_url, id);
+    })().then(
+        () => {
+            console.log("Mol* initialization ✅");
+        },
+        (error) => {
+            console.log("Mol* initialization ❌", error);
+        }
+    );
 }
 
+async function load(structure_url, id) {
+    const first_example = "P34712_7.2_4";
 
-function init_results(idd, chg_range) {
+    await molstar.load(structure_url);
 
-    const $select = idd
+    const cartoonOff = switchOffCartoonView();
+    if (cartoonOff) {
+        await molstar.type.ballAndStick();
+        document.getElementById("view_bas").setAttribute("checked", "true");
+    } else if (id === first_example) {
+        await molstar.type.surface();
+        document.getElementById("view_surface").setAttribute("checked", "true");
+    } else {
+        await molstar.type.default();
+        document.getElementById("view_cartoon").setAttribute("checked", "true");
+    }
+    document.getElementById("colors_charges").setAttribute("checked", "true");
 
-    let $min_value = $('#min_value');
-    let $max_value = $('#max_value');
+    let maxAbsoluteRelativeCharge = Number(
+        molstar.charges.getRelativeCharge().toFixed(3)
+    );
+    await updateSliderMax(maxAbsoluteRelativeCharge);
+    await updateCharge(maxAbsoluteRelativeCharge);
 
-        const id = $select;
-        $.ajax({
-            url: get_format_url,
-            success: function (format) {
-                LiteMolChargesViewerEventQueue.send("lm-load-molecule", {
-                    structure_url: get_structure_url,
-                    charges_url: get_charges_url,
-                    structure_format: format,
-                    charges_format: 'TXT'
-                });
-            }
-        })
-
-
-
-
-        $('input:radio[name=colors]').prop('disabled', false);
-
-        if ($('input[name=colors]:checked').val() === 'Relative') {
-            $min_value.val(-chg_range);
-            $max_value.val(chg_range);
-            $min_value.trigger('input');
-        }
-
-
-    $('#min_value, #max_value').on('input', function () {
-        update_litemol_colors(parseFloat($('#min_value').val()), parseFloat($('#max_value').val()));
-        $min_value.attr('max', $max_value.val());
-        $max_value.attr('min', $min_value.val());
-    });
-
-    let $colors = $('input[name=colors]');
-    $colors.on('change', function () {
-        let coloring = $('input[name=colors]:checked').val();
-        if (coloring === 'Relative') {
-            LiteMolChargesViewerEventQueue.send('lm-use-default-themes', {value: false});
-            const id = $select;
-            $min_value.val(-chg_range);
-            $max_value.val(chg_range);
-
-            update_litemol_colors(null, null);
-            $min_value.prop('disabled', true);
-            $max_value.prop('disabled', true);
-        } else if (coloring === 'Absolute') {
-            LiteMolChargesViewerEventQueue.send('lm-use-default-themes', {value: false});
-            $min_value.prop('disabled', false);
-            $max_value.prop('disabled', false);
-            $min_value.trigger('input');
-        } else {
-            /* Coloring by elements */
-            LiteMolChargesViewerEventQueue.send('lm-use-default-themes', {value: true});
-
-        }
-    });
-
-
-    let $view = $('input[name=view]');
-    $view.on('change', function () {
-        let v = $('input[name=view]:checked').val();
-        if (v === 'Cartoon') {
-            LiteMolChargesViewerEventQueue.send('lm-switch-to-cartoons');
-        } else if (v === 'Balls and sticks') {
-            LiteMolChargesViewerEventQueue.send('lm-switch-to-bas');
-        } else {
-            /* Surface */
-            LiteMolChargesViewerEventQueue.send('lm-switch-to-surface');
-        }
-    });
-    $colors.filter(':checked').trigger('change');
+    mountTypeControls();
+    mountColorControls();
+    mountRangeControls();
+    addEventListeners();
 }
 
+function switchOffCartoonView() {
+    const view = document.getElementById("view_cartoon");
+    if (!view) return false;
+    if (!molstar.type.isDefaultApplicable()) {
+        view.setAttribute("disabled", "true");
+        return true;
+    } else {
+        view.removeAttribute("disabled");
+        return false;
+    }
+}
 
+async function updateSliderMax(max) {
+    const slider = document.getElementById("input_range_max_charge");
+    if (!slider) return;
+    const old_value = Number(slider.value);
+    slider.max = max.toFixed(3);
+    if (old_value > max) {
+        await updateCharge(max);
+    }
+}
+
+async function updateCharge(chg) {
+    if (isNaN(chg)) return;
+    const header = document.getElementById("input_label_max_charge");
+    const slider = document.getElementById("input_range_max_charge");
+    if (!header || !slider) return;
+    charge = Number(chg.toFixed(3));
+    header.innerText = `Charge range: ${charge.toFixed(3)}`;
+    slider.value = `${charge.toFixed(3)}`;
+    await molstar.color.absolute(charge);
+}
+
+function mountTypeControls() {
+    const cartoon = document.getElementById("view_cartoon");
+    const surface = document.getElementById("view_surface");
+    const bas = document.getElementById("view_bas");
+    if (!cartoon || !surface || !bas) return;
+    cartoon.onclick = async () => await molstar.type.default();
+    surface.onclick = async () => await molstar.type.surface();
+    bas.onclick = async () => await molstar.type.ballAndStick();
+}
+
+function mountColorControls() {
+    const structure = document.getElementById("colors_structure");
+    const absolute = document.getElementById("colors_charges");
+    if (!structure || !absolute) return;
+    structure.onclick = async () => await molstar.color.default();
+    absolute.onclick = async () => {
+        const relativeCharge = molstar.charges.getRelativeCharge();
+        await updateSliderMax(relativeCharge);
+        await updateCharge(relativeCharge);
+    };
+}
+
+function mountRangeControls() {
+    const slider = document.getElementById("input_range_max_charge");
+    const input = document.getElementById("input_text_max_charge");
+    if (!slider || !input) return;
+    slider.oninput = async () => {
+        const slider = document.getElementById("input_range_max_charge");
+        if (!slider) return;
+        const chg = Number(slider.value);
+        await updateCharge(chg);
+    };
+    input.oninput = async () => {
+        const input = document.getElementById("input_text_max_charge");
+        if (!input) return;
+        const value = input.value;
+        const placeholder = input.placeholder;
+        const defaultValue = !isNaN(Number(placeholder))
+            ? Number(placeholder)
+            : 0;
+        const parsed =
+            value !== "" && !isNaN(Number(value))
+                ? Number(value)
+                : defaultValue;
+        await updateSliderMax(parsed);
+    };
+}
