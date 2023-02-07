b46761377574ab9a567cd3a34f0209fc488a7e1f
Dominik Tichy <tichydominik451@gmail.com>
2023-02-07 19:34:28 +0000

    feat: added viewer interactions

--- app/static/main.js
+++ app/static/main.js
@@ -1,6 +1,5 @@
 "use strict";
 
-let charge;
 let molstar;
 
 function init_results(structure_url, id) {
@@ -9,6 +8,7 @@ function init_results(structure_url, id) {
         await load(structure_url, id);
     })().then(
         () => {
+            // TODO: remove
             console.log("Mol* initialization ✅");
         },
         (error) => {
@@ -20,64 +20,22 @@ function init_results(structure_url, id) {
 async function load(structure_url, id) {
     const first_example = "P34712_7.2_4";
 
+    setDefaultRadioButtons();
+
     await molstar.load(structure_url);
 
-    const cartoonOff = switchOffCartoonView();
-    if (cartoonOff) {
-        await molstar.type.ballAndStick();
-        document.getElementById("view_bas").setAttribute("checked", "true");
-    } else if (id === first_example) {
-        await molstar.type.surface();
-        document.getElementById("view_surface").setAttribute("checked", "true");
-    } else {
-        await molstar.type.default();
-        document.getElementById("view_cartoon").setAttribute("checked", "true");
-    }
-    document.getElementById("colors_charges").setAttribute("checked", "true");
+    if (id === first_example) await molstar.type.surface();
+    else await molstar.type.default();
 
-    let maxAbsoluteRelativeCharge = Number(
-        molstar.charges.getRelativeCharge().toFixed(3)
-    );
-    await updateSliderMax(maxAbsoluteRelativeCharge);
-    await updateCharge(maxAbsoluteRelativeCharge);
+    updateRelativeColor();
 
     mountTypeControls();
     mountColorControls();
-    mountRangeControls();
-    addEventListeners();
 }
 
-function switchOffCartoonView() {
-    const view = document.getElementById("view_cartoon");
-    if (!view) return false;
-    if (!molstar.type.isDefaultApplicable()) {
-        view.setAttribute("disabled", "true");
-        return true;
-    } else {
-        view.removeAttribute("disabled");
-        return false;
-    }
-}
-
-async function updateSliderMax(max) {
-    const slider = document.getElementById("input_range_max_charge");
-    if (!slider) return;
-    const old_value = Number(slider.value);
-    slider.max = max.toFixed(3);
-    if (old_value > max) {
-        await updateCharge(max);
-    }
-}
-
-async function updateCharge(chg) {
-    if (isNaN(chg)) return;
-    const header = document.getElementById("input_label_max_charge");
-    const slider = document.getElementById("input_range_max_charge");
-    if (!header || !slider) return;
-    charge = Number(chg.toFixed(3));
-    header.innerText = `Charge range: ${charge.toFixed(3)}`;
-    slider.value = `${charge.toFixed(3)}`;
-    await molstar.color.absolute(charge);
+function setDefaultRadioButtons() {
+    document.getElementById("view_surface").setAttribute("checked", "true");
+    document.getElementById("colors_relative").setAttribute("checked", "true");
 }
 
 function mountTypeControls() {
@@ -92,38 +50,44 @@ function mountTypeControls() {
 
 function mountColorControls() {
     const structure = document.getElementById("colors_structure");
-    const absolute = document.getElementById("colors_charges");
-    if (!structure || !absolute) return;
-    structure.onclick = async () => await molstar.color.default();
-    absolute.onclick = async () => {
-        const relativeCharge = molstar.charges.getRelativeCharge();
-        await updateSliderMax(relativeCharge);
-        await updateCharge(relativeCharge);
-    };
+    const relative = document.getElementById("colors_relative");
+    const absolute = document.getElementById("colors_absolute");
+    const range = document.getElementById("max_value");
+    if (!structure || !relative || !absolute) return;
+    structure.onclick = async () => await updateDefaultColor();
+    relative.onclick = async () => await updateRelativeColor();
+    absolute.onclick = async () => await updateAbsoluteColor();
+    range.oninput = async () => await updateRange();
 }
 
-function mountRangeControls() {
-    const slider = document.getElementById("input_range_max_charge");
-    const input = document.getElementById("input_text_max_charge");
-    if (!slider || !input) return;
-    slider.oninput = async () => {
-        const slider = document.getElementById("input_range_max_charge");
-        if (!slider) return;
-        const chg = Number(slider.value);
-        await updateCharge(chg);
-    };
-    input.oninput = async () => {
-        const input = document.getElementById("input_text_max_charge");
-        if (!input) return;
-        const value = input.value;
-        const placeholder = input.placeholder;
-        const defaultValue = !isNaN(Number(placeholder))
-            ? Number(placeholder)
-            : 0;
-        const parsed =
-            value !== "" && !isNaN(Number(value))
-                ? Number(value)
-                : defaultValue;
-        await updateSliderMax(parsed);
-    };
+async function updateDefaultColor() {
+    const input = document.getElementById("max_value");
+    if (!input) return;
+    input.setAttribute("disabled", "true");
+    await molstar.color.default();
+}
+
+async function updateRelativeColor() {
+    const input = document.getElementById("max_value");
+    if (!input) return;
+    input.setAttribute("disabled", "true");
+    const charge = await molstar.charges.getRelativeCharge();
+    input.value = charge.toFixed(3);
+    // ? updates viewer each time the radio button is clicked ?
+    await molstar.color.relative();
+}
+
+async function updateAbsoluteColor() {
+    const input = document.getElementById("max_value");
+    if (!input) return;
+    input.removeAttribute("disabled");
+}
+
+async function updateRange() {
+    const input = document.getElementById("max_value");
+    if (!input) return;
+    const value = Number(input.value);
+    console.log(value);
+    if (isNaN(value)) return;
+    await molstar.color.absolute(value);
 }
